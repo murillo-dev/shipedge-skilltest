70d354545d15532f28459884f6c99c110b02f889
Israel Murillo <murricito@gmail.com>
2023-12-07 16:41:47 -0400

    clean code

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,28 +1,39 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTableModule, MatTable } from '@angular/material/table';
-import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
-import { MatSortModule, MatSort } from '@angular/material/sort';
-import { PostsDataSource, PostsItem } from './posts-datasource';
+import {
+  Component,
+  WritableSignal,
+  inject,
+  signal,
+} from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { PostsService } from '../../services/posts.service';
+import { CommonModule } from '@angular/common';
 
+export interface PostsItem {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+}
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss'],
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+  imports: [MatTableModule, CommonModule],
 })
-export class PostsComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<PostsItem>;
-  dataSource = new PostsDataSource();
+export class PostsComponent {
 
-  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  private postsService = inject(PostsService);
+
+  dataSource: WritableSignal<PostsItem[]> = signal([]);
+
   displayedColumns = ['id', 'userId', 'title', 'body'];
 
-  ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+  constructor() {
+    this.postsService.getPosts().subscribe((request: any) => {
+      this.dataSource.set(request);
+    });
   }
 }
